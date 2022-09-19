d1337cf2a3050bbe72a5fbf8f2806c5242925f67
MariamDabous <mariam.dabous@hotmail.com>
2022-09-19 15:54:06 +0300

    final

---
+++ Counter/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template,redirect,request, session
+app = Flask(__name__)
+app.secret_key='ggggggggggggggggggg'
+
+
+@app.route('/')          
+def counter():
+    if 'visits' not in session:
+        session['visits']=0
+    else:
+        session['visits']+=1
+    
+    return render_template('index.html', num=session['visits'])  
+
+@app.route('/destroy_session')          
+def destroy():
+    session.clear()
+
+    return render_template('index.html') 
+
+
+if __name__=="__main__":   
+    app.run(debug=True)    
