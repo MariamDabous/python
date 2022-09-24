018e6d9c3c1366c2a12ae596acf8b2143027663a
MariamDabous <mariam.dabous@hotmail.com>
2022-09-24 20:51:13 +0300

    final

--- Django/time_display/app_one/views.py
+++ Django/time_display/app_one/views.py
@@ -1,12 +1,13 @@
+from email.utils import localtime
 from django.shortcuts import render, HttpResponse
 
 
-from time import gmtime, strftime
+from time import localtime, strftime
     
 def index(request):
     context = {
-        "date": strftime("%b %d, %Y ", gmtime()),
-        "time": strftime("%I:%M %p", gmtime())
+        "date": strftime("%b %d, %Y ", localtime()),
+        "time": strftime("%I:%M %p", localtime())
     }
     return render(request,'index.html', context)
 
